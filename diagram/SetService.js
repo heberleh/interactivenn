c5b55b26034a249f25a07934bd95b67cf072d114
Jay Clark <jay@jayeclark.dev>
2022-01-10 15:06:38 -0400

    Migrate SetService
    Signed-off-by: Jay Clark <jay@jayeclark.dev>

---
+++ diagram/SetService.js
@@ -0,0 +1,22 @@
+export default class SetService{
+
+  getUnionOfAllSets(setsArray) {
+      const totalSet = new Set();
+      setsArray.forEach(vectorSet =>{
+          vectorSet.forEach(element=> totalSet.add(element));
+      })
+      return totalSet;
+  }
+  
+  getUnionOfAllSetsArray(setsArray){
+      return Array.from(this.getUnionOfAllSets(setsArray));
+  }
+
+  getPercentageString(partSize, totalSize){
+      if(totalSize===0||partSize===0){
+          return "0.0";
+      }
+      return (partSize*100/totalSize).toFixed(1);
+  }
+
+}
