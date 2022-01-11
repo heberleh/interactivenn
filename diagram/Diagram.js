9809b32bd021f4a6a386f884229feaa8d2f96868
Jay Clark <jay@jayeclark.dev>
2022-01-11 15:00:20 -0400

    Remove unnecessary code
    Signed-off-by: Jay Clark <jay@jayeclark.dev>

--- diagram/Diagram.js
+++ diagram/Diagram.js
@@ -1,29 +1,5 @@
 import setService from "./SetService.js";
 
-var importedNode;
-var originalAllSetsNames = ["A", "B", "C", "D", "E", "F"];
-var allSetsNames = ["A", "B", "C", "D", "E", "F"]; //currently selected!! depends on nWays
-var sets = {};
-var slide = null;
-var maxIndex = 0;
-var currentIndex = maxIndex;
-var examplelist = [];
-examplelist["2"] = "ab";
-examplelist["3"] = "ab;ca";
-examplelist["4"] = "ca,db; ab";
-examplelist["5"] = "ab; cd; be,ac";
-examplelist["6"] = "ab; cd,ef; abc,de; abcd,ef";
-
-var tree = null;
-var maxLevel = 0;
-var currentLevel = maxLevel;
-var exampletree = [];
-exampletree["2"] = "(A,B)";
-exampletree["3"] = "((A,B),C)";
-exampletree["4"] = "((A,(B,C)),D)";
-exampletree["5"] = "((A,((D,E),C)),B)";
-exampletree["6"] = "((A,((D,(F,E)),C)),B)";
-
 // TODO: refactor to an exportable class
 // TODO: refactor so that only svg elements are manipulated by loadDiagram and javascript .js manipulates the dom elements
 // TODO: remove merging logic and migrate to javascript.js
@@ -62,7 +38,7 @@ export default class Diagram {
    * @description Loads a diagram and process everything based on it.
    * @param {string} path The path of a .svg diagram
    */
-  loadNewDiagramList(path) {
+  loadNewDiagram(path) {
     d3.xml(path, "image/svg+xml", function (xml) {
       try {
         importedNode = document.importNode(xml.documentElement, true);
@@ -141,49 +117,6 @@ export default class Diagram {
 
       updateActiveSetsList();
 
-      if (merging) {
-        updateDiagram();
-      } else {
-        d3.selectAll(".elipse").attr("visibility", "hidden");
-        d3.selectAll("text").attr("visibility", "hidden");
-      }
-      var delay = 0;
-      var duration = 0;
-
-      d3.selectAll(".elipse")
-        .transition()
-        .delay(delay)
-        .duration(duration)
-        .attr("visibility", "visible");
-      d3.selectAll("text")
-        .transition()
-        .delay(delay)
-        .attr("visibility", "visible")
-        .style("font-family", "Arial");
-
-      d3.select("#updateDiagrambutton")
-        .transition()
-        .delay(delay)
-        .style("display", "inline");
-
-      //document.getElementById("updateMerge").disabled = false;
-
-      if (merging) {
-        //alert("merging");
-        if (currentIndex >= slide.getSize() - 1) {
-          document.getElementById("upMerge").disabled = true;
-        } else {
-          document.getElementById("upMerge").disabled = false;
-        }
-        if (currentIndex <= 0) {
-          document.getElementById("downMerge").disabled = true;
-        } else {
-          document.getElementById("downMerge").disabled = false;
-        }
-      } else {
-        document.getElementById("upMerge").disabled = true;
-        document.getElementById("downMerge").disabled = true;
-      }
       updateLabelsSizes();
       updateSetsLabelsList();
       $(document).ready(updateDiagram());
@@ -194,13 +127,6 @@ export default class Diagram {
       $(document).ready(updateColors());
       $(document).ready(updateOpacity());
 
-      // d3.selectAll("path").on('mouseover', function (d) {
-      //     console.log("mouseover", d);
-      //     d3.select(this).style({"fill-opacity": globalOpacity + 0.2});
-      // })
-      // d3.selectAll("path").on('mouseout', function (d) {
-      //     d3.select(this).style({"fill-opacity": globalOpacity});
-      // })
       setMouseOverIntersectionsLabels();
     });
   }
@@ -208,7 +134,7 @@ export default class Diagram {
   /**
    * @description Updates the sets and sets' labels given the texts inputs in the web interface.
    */
-  updateActiveSetsList() {
+  updateActiveSets() {
     for (var i = 0; i < originalAllSetsNames.length; i++) {
       modified = true;
       updateSets(originalAllSetsNames[i]);
@@ -220,7 +146,7 @@ export default class Diagram {
    * @description Updates the label of a set in the diagram given the label typed by the user on the web interface.
    * @param {string} setID The set ID.
    */
-  updateSetLabelList(setID) {
+  updateSetLabel(setID) {
     var text = document.getElementById("name" + setID).value;
     d3.select("#label" + setID).text(text.replace(/:/g, "-"));
     //console.log(updateDendrogram, typeof updateDendrogram);
@@ -230,156 +156,10 @@ export default class Diagram {
   /**
    * @description Updates the label of all sets in the diagram given the labels typed by the user on the web interface.
    */
-  updateSetsLabelsList() {
+  updateSetsLabels() {
     for (var i = 0; i < nWay; i++) {
       updateSetLabelList(allPossibleSetsNames[i]);
     }
-  }
-
-  /**
-   * @description Loads a diagram and process everything based on it.
-   * @param {string} path The path of a .svg diagram
-   */
-  loadNewDiagramTree(path) {
-    d3.xml(path, "image/svg+xml", function (xml) {
-      try {
-        importedNode = document.importNode(xml.documentElement, true);
-      } catch (err) {
-        alert(
-          "\nDiagram not found: " +
-            path +
-            "\n\n Error: " +
-            err.message +
-            ". Please report it to henry at icmc.usp.br\n\n"
-        );
-      }
-
-      diagramReady = false;
-
-      var node = document.getElementById("diagram");
-
-      if (node != null) {
-        node.parentNode.removeChild(node);
-      }
-
-      d3.select("#diagramframe").node().appendChild(importedNode);
-      diagramReady = true;
-
-      d3.select(importedNode)
-        .attr("viewBox", "0 0 700 700")
-        .attr("align", "center")
-        .attr("width", "600")
-        .attr("height", "578");
-
-      var vtemp = d3.selectAll("text")[0];
-
-      labelsDiagram = [];
-      for (var j = 0; j < vtemp.length; j++) {
-        var str = vtemp[j].textContent;
-        console.log(vtemp[j].class);
-        if (
-          vtemp[j].id != null &&
-          vtemp[j].id != "" &&
-          vtemp[j].id.substring(0, 5) != "label" &&
-          vtemp[j].id.substring(0, 5) != "total" &&
-          str != "(total)"
-        ) {
-          labelsDiagram.push(str);
-
-          var el = document.getElementById(str);
-          el.onclick = function (e) {
-            var node = e.target;
-            var id = node.id.toUpperCase();
-
-            var textName = "<b>[";
-            textName = textName + document.getElementById("name" + id[0]).value;
-            for (var i = 1; i < id.length; i++) {
-              textName =
-                textName +
-                "] and [" +
-                document.getElementById("name" + id[i]).value;
-            }
-            textName = textName + "]";
-            textName = textName.replace(/:/g, "-");
-            var strIntersection = textName + ":</b></br>";
-
-            for (var t = 0; t < intersectionsSet[id].length; t++) {
-              strIntersection =
-                strIntersection + intersectionsSet[id][t] + "</br>";
-            }
-
-            var htmlp = "<p>" + strIntersection + "</p>";
-
-            var newWindow = window.open(
-              "",
-              id,
-              "scrollbars=yes,toolbar=no,menubar=no,status=no,directories=no,location=no,width=400,height=400"
-            );
-            newWindow.document.write(htmlp);
-          };
-        }
-      }
-
-      updateActiveSetsTree();
-      if (merging) {
-        updateDiagram();
-      } else {
-        d3.selectAll(".elipse").attr("visibility", "hidden");
-        d3.selectAll("text").attr("visibility", "hidden");
-      }
-      var delay = 0;
-      var duration = 0;
-
-      d3.selectAll(".elipse")
-        .transition()
-        .delay(delay)
-        .duration(duration)
-        .attr("visibility", "visible");
-      d3.selectAll("text")
-        .transition()
-        .delay(delay)
-        .attr("visibility", "visible")
-        .style("font-family", "Arial");
-
-      d3.select("#updateDiagrambutton")
-        .transition()
-        .delay(delay)
-        .style("display", "inline");
-
-      //document.getElementById("updateMerge").disabled = false;
-
-      if (merging) {
-        if (currentLevel >= tree.getMaxLevel()) {
-          document.getElementById("downMerge").disabled = true;
-        } else {
-          document.getElementById("downMerge").disabled = false;
-        }
-        if (currentLevel <= 0) {
-          document.getElementById("upMerge").disabled = true;
-        } else {
-          document.getElementById("upMerge").disabled = false;
-        }
-      } else {
-        document.getElementById("upMerge").disabled = true;
-        document.getElementById("downMerge").disabled = true;
-      }
-      updateLabelsSizes();
-      updateSetsLabelsTree();
-      $(document).ready(updateDiagram());
-      if (!merging) {
-        $(document).ready(updateColorBox());
-      }
-      $(document).ready(updateColors());
-      $(document).ready(updateOpacity());
-
-      // d3.selectAll("path").on('mouseover', function (d) {
-      //     d3.select(this).style({"fill-opacity": globalOpacity + 0.2});
-      // })
-      // d3.selectAll("path").on('mouseout', function (d) {
-      //     d3.select(this).style({"fill-opacity": globalOpacity});
-      // })
-      setMouseOverIntersectionsLabels();
-    });
   }
 
   setMouseOverIntersectionsLabels() {
@@ -492,24 +272,6 @@ export default class Diagram {
   }
 
   /**
-   * @description Updates the label of a set in the diagram given the label typed by the user on the web interface.
-   * @param {string} setID The set ID.
-   */
-  updateSetLabelTree(setID) {
-    var text = document.getElementById("name" + setID).value;
-    d3.select("#label" + setID).text(text.replace(/:/g, "-"));
-  }
-
-  /**
-   * @description Updates the label of all sets in the diagram given the labels typed by the user on the web interface.
-   */
-  updateSetsLabelsTree() {
-    for (var i = 0; i < nWay; i++) {
-      updateSetLabelTree(allPossibleSetsNames[i]);
-    }
-  }
-
-  /**
    * @description Updates the values (numbers) that are shown in the diagram. Identifies all the possible intersections and set a new numeric text to it.
    */
   updateDiagram() {
@@ -561,14 +323,4 @@ export default class Diagram {
     }
   }
 
-  /**
-   * @description Updates the sets and sets' labels given the texts inputs in the web interface.
-   */
-  updateActiveSetsTree() {
-    for (var i = 0; i < originalAllSetsNames.length; i++) {
-      modified = true;
-      updateSets(originalAllSetsNames[i]);
-      updateSetLabelTree(originalAllSetsNames[i]);
-    }
-  }
 }
