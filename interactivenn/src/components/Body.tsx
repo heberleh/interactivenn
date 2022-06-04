0578eb92f702c29b7564a2d2508b12bcb7ed911e
Henry Heberle <heberleh.public@gmail.com>
2022-06-04 13:12:27 +0200

    Body, Header, Footer structure

---
+++ interactivenn/src/components/Body.tsx
@@ -0,0 +1,12 @@
+import { Copyright } from "@mui/icons-material";
+import { Box, Container, Typography } from "@mui/material";
+
+const Body = () => {
+  return (
+    <Container maxWidth="sm">
+      <Box sx={{ my: 4 }}></Box>
+    </Container>
+  );
+};
+
+export default Body;
